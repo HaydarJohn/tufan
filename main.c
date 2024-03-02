5a39560a68f3dd73793a85f4669c2c20db6abca0
haydarjohn <haydarcan_2003@hotmail.com>
2024-03-02 14:58:25 +0300

    Removed libraries and emty files.New design is needed

--- main.c
+++ main.c
@@ -2,12 +2,5 @@
 
 int main()
 {
-
-    logger(LOG_LEVEL_ERROR,"flkjaljfasklj");
-    logger(LOG_LEVEL_INFO,"flkjaljfasklj");
-    logger(LOG_LEVEL_INFO,"flkjaljfasklj");
-    logger(LOG_LEVEL_ERROR,"flkjaljfasklj");
-    logger(LOG_LEVEL_ERROR,"flkjaljfasklj");
-    logger(LOG_LEVEL_WARNING,"flkjaljfasklj");
-    logger(LOG_LEVEL_ERROR,"flkjaljfasklj");
+    
 }
